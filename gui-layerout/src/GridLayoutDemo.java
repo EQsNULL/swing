38705df4568d2ed53e274728f1aedefd06cb2b3e
EQsNULL <3470332054@qq.com>
2019-07-18 10:48:42 +0800

    GridLayout，即网格布局器

--- gui-layerout/src/GridLayoutDemo.java
+++ gui-layerout/src/GridLayoutDemo.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GridLayoutDemo {
     public static void main(String[] args) {
+        JFrame f = new JFrame("LoL");
+        f.setSize(400, 300);
+        f.setLocation(200, 200);
+        // 设置布局器为GridLayerout，即网格布局器
+        // 该GridLayerout的构造方法表示该网格是2行3列
+        f.setLayout(new GridLayout(2, 3));
 
+        JButton b1 = new JButton("洪七");
+        JButton b2 = new JButton("段智兴");
+        JButton b3 = new JButton("欧阳锋");
+        JButton b4 = new JButton("黄药师");
+        JButton b5 = new JButton("周伯通");
+        JButton b6 = new JButton("老干妈");
+
+        f.add(b1);
+        f.add(b2);
+        f.add(b3);
+        f.add(b4);
+        f.add(b5);
+        f.add(b6);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setVisible(true);
     }
 }
