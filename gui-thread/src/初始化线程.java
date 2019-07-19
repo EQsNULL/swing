974c6a242ac3181f8d5ae745dfae4cc3dbe0de7d
EQsNULL <3470332054@qq.com>
2019-07-19 10:58:30 +0800

    当前线程是否是 事件调度线程: false

---
+++ gui-thread/src/初始化线程.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+public class 初始化线程 {
+    public static void main(String[] args) {
+        new TestFrame().setVisible(true);
+
+//      SwingUtilities.invokeLater(new Runnable() {
+//          public void run() {
+//              new TestFrame().setVisible(true);
+//          }
+//      });
+    }
+
+    static class TestFrame extends JFrame {
+        public TestFrame() {
+            setTitle("LoL");
+
+            setSize(400, 300);
+
+            setLocation(200, 200);
+
+            setLayout(null);
+
+            JButton b = new JButton("一键秒对方基地挂");
+
+            b.setBounds(50, 50, 280, 30);
+
+            add(b);
+
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            setVisible(true);
+
+            System.out.println("当前线程是否是 事件调度线程: " + SwingUtilities.isEventDispatchThread());
+
+        }
+    }
+}
