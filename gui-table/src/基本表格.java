148e165c997a48be46ba6a220383b65bca07fcf4
EQsNULL <3470332054@qq.com>
2019-07-19 14:51:29 +0800

    显示一个Table需要两组数据 1. 一维数组： String[]columnNames 表示表格的标题 2. 二维数组： String[][] heros 表格中的内容

---
+++ gui-table/src/基本表格.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class 基本表格 {
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame("LoL");
+        f.setSize(400, 300);
+        f.setLocation(200, 200);
+        f.setLayout(new BorderLayout());
+
+        // 表格上的title
+        String[] columnNames = new String[] { "id", "name", "hp", "damage" };
+        // 表格中的内容，是一个二维数组
+        String[][] heros = new String[][] { { "1", "盖伦", "616", "100" },
+                { "2", "提莫", "512", "102" }, { "3", "奎因", "832", "200" } };
+        JTable t = new JTable(heros, columnNames);
+        f.add(t, BorderLayout.CENTER);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setVisible(true);
+    }
+}
